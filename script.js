3144024c02979b16c6064c45e59895be1dec69cf
dsshp7 <saisreehariprasad@gmail.com>
2023-08-29 21:02:57 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("My Name is D.Sai Sreehari Prasad")
+let a = 3
+let b = 4
+
+var city = "Vishakapatnam"
+
+console.log("addition: " + String(a + b))
+console.log("subtraction: " + String(a - b))
+console.log("multiplication: " + String(a * b))
+console.log("division: ", + String(a / b))
+console.log(city)
+
+function greet(name) {
+    console.log("Hello " + name + "!")
+}
+
+greet("sai")
+
+function addition(a, b) {
+    console.log("addition of given two numbers is:" + String(a+b))
+}
+
+addition(2, 3)
+addition(4, 5) 
+
+let birthYear = 1995
+let favouriteFoods = ["Biryani", "Pongal", "pesarattu"]
+let book = {
+    title: "Art of Living",
+    author: "sadguru",
+    publicationYear: 2015
+}
+
+console.log(birthYear,favouriteFoods,book)
+
